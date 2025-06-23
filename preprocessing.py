906dc4854c6c2e26f5f26fe9f8ba98e6548ad7ea
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-06-23 17:39:56 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,27 @@
 import pandas as pd
-from surprise import Dataset, Reader
 
-def load_data(file_path):
-    df = pd.read_csv(file_path)
-    reader = Reader(rating_scale = (1,5))
-    data = Dataset.load_from_df(df[['user_id', 'item_id', 'rating']], reader)
-    return data
+def load_ratings(file_path):
+
+    """
+    load and preprocess the user rating data.
+    Return a user-item matrix(rows = users, columns = items)
+    :param file_path:
+    :return:
+    """
+    try:
+
+        df = pd.read_csv(file_path)
+        #Basic Checkes
+
+        df.dropana(inplace = True)
+        df = df[df['rating']>0] #Removes 0 or negative rating if any
+
+        user_item_matrix = df.pivot_table(
+            index = 'user_id',
+            columns = 'item_id',
+            values = 'rating'
+        ).fillna(0)
+        return user_item_matrix
+    except Exception as e:
+        print("Error loading or preprocessing data", e)
+        return None
